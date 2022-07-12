4650a71d17135408ed8311fd1955cbe214b1b8ba
David Faibil <100334861+davefcodes@users.noreply.github.com>
2022-07-12 17:20:43 -0400

    Update findNeedle.py

--- 8kyu/findNeedle.py
+++ 8kyu/findNeedle.py
@@ -1,1 +1,16 @@
+# Instructions 
+
+# Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+# After your function finds the needle it should return a message (as a string) that says: 
+# "found the needle at position " plus the index it found the needle, so
+
+# Example
+# ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+# My solution 
+
+def find_needle(haystack):
+    # your code here
+    return "found the needle at position {}".format(haystack.index("needle"))
 
